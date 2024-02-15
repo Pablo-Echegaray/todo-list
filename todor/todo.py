92138badb11f1cfdcccca6f5d1f3ff2ae963732d
Pablo-Echegaray <pabloechegaray95@gmail.com>
2024-02-14 22:35:05 -0300

    Listar tareas.

--- todor/todo.py
+++ todor/todo.py
@@ -1,15 +1,17 @@
 from flask import Blueprint, render_template
+from todor.auth import login_required
+from .models import Todo, User
+from todor import db
 
 # Vistas con Blueprint.
-
 bp = Blueprint('todo', __name__, url_prefix='/todo')
 
-from todor.auth import login_required
 
 @bp.route('/list')
 @login_required
 def index():
-    return render_template('todo/index.html')
+    todos = Todo.query.all()
+    return render_template('todo/index.html', todos=todos)
 
 @bp.route('/create')
 def create():
